cb6250720148d7d6fc76c40ab6980a26e0803eff
Sathurshan <jsathu07@gmail.com>
2024-10-21 16:30:49 +0530

    chore: configure project

---
+++ backend/src/test/java/org/sathu/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sathu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
